b3ea90990b362fa85666b5aa9eb4b72b034340de
tiago-yoneda <xyonedax@gmail.com>
2021-10-07 15:40:32 -0300

    atualizando alguns arquivos

--- src/models/connection.js
+++ src/models/connection.js
@@ -6,7 +6,8 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
+// Substituir aqui a url do banco de dados rodando em sua maquina
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://localhost:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
 
 let db = null;
